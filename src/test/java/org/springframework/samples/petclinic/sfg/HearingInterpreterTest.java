202041c961ba941801d830fa28b9cbf895a407e0
Ernesto Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-02-12 19:56:04 +0000

    Spring Framework Testing Context

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
@@ -1,18 +1,19 @@
 package org.springframework.samples.petclinic.sfg;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
 public class HearingInterpreterTest {
 
+    @Autowired
     HearingInterpreter hearingInterpreter;
-
-    @Before
-    public void setUp() throws Exception {
-        hearingInterpreter = new HearingInterpreter(new LaurelWordProducer());
-    }
 
     @Test
     public void whatIHeard() {
